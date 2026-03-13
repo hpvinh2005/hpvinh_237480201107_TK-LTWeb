abd3e1c09ffa5d883a3f1f5f953844066e512b37
huynh phuoc vinh <huynhphuocvinh2021@gmail.com>
2026-03-13 14:00:48 +0700

    13/3/2026

---
+++ Chuong4/1a.js
@@ -0,0 +1,9 @@
+function nhapthongtin()
+{
+    var ten = prompt ("nhập tên:");
+    var tuoi = prompt ("nhập tuổi:");
+
+    document.write("ten" + ten + "<br>");
+    document.write("tuoi" + tuoi);
+}
+nhapthongtin()
